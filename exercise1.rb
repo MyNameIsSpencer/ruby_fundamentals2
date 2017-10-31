38070cd249f7c638657404fbd163fc5aa9caef7c
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-10-31 16:21:40 -0400

    exercise 1 complete

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,10 @@
-def first_method
-  return 1 + 1
+def double(my_number)
+  return my_number * 2
 end
+
+puts "#{double(13)}"
+
+puts "#{double(78)}"
+
+user_a = gets.chomp.to_f
+puts "#{double(user_a)}"
