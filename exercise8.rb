028192421f507756e9daeda55ab1b4291cf70dfb
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-10-31 19:42:35 -0400

    Exercise 8 final

--- exercise8.rb
+++ exercise8.rb
@@ -16,7 +16,17 @@ speed2 = test_1(2)
 speed3 = test_1(3)
 
 
-
+if speed3 > speed2 && speed3 > speed1
+  puts "Person 3 was the fastest at #{speed3} m/s"
+elsif speed2 > speed3 && speed2 > speed1
+  puts "Person 2 was the fastest at #{speed2} m/s"
+elsif speed1 > speed3 && speed1 > speed2
+  puts "Person 1 was the fastest at #{speed1} m/s"
+elsif speed1 == speed2 && speed2 == speed3
+  puts "Everyone tied at #{speed1} m/s"
+else
+  puts "Well done everyone!"
+end
 
 
 
